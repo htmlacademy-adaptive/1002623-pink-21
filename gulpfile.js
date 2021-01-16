7f0926a39fa2382bab7aa4a0328efd9e081ad076
Rashchenia_Vitali <rvvital@mail.ru>
2021-01-16 22:16:14 +0300

    Исправление gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -23,41 +23,26 @@ const styles = () => {
       autoprefixer(),
       csso()
     ]))
-    .pipe(sourcemap.write("."))
     .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
 
-//Images
-
-const images = () => {
-  return gulp.src("source/img/**/*.{jpg,png,svg}")
-    .pipe(imagemin([
-      imagemin.mozjpeg({progressive: true}),
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.svgo()
-    ]))
-    .pipe(gulp.dest("build/img"));
-}
-
-exports.images = images;
-
-
-//HTML
+// HTML
 
 const html = () => {
   return gulp.src("source/*.html")
-  .pipe(htmlmin({collapseWhitespace: true}))
-  .pipe(gulp.dest("build"))
-  .pipe(sync.stream());
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
 }
 
-//Scripts
+// Scripts
+
 const scripts = () => {
-  return gulp.src("source/js/*.js")
+  return gulp.src("source/js/script.js")
     .pipe(uglify())
     .pipe(rename("script.min.js"))
     .pipe(gulp.dest("build/js"))
@@ -66,28 +51,40 @@ const scripts = () => {
 
 exports.scripts = scripts;
 
+// Images
 
-//Copy
-const copy= (done) => {
-  return gulp.src([
+const images = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.images = images;
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
     "source/fonts/*.{woff2,woff}",
-    "source/img/**/*.{jpg,png,svg}"
-  ],
-  {
+    "source/img/**/*.{jpg,png,svg}",
+  ], {
     base: "source"
   })
-  .pipe(gulp.dest("build"));
+    .pipe(gulp.dest("build"))
   done();
 }
 
 exports.copy = copy;
 
-//Clean
+// Clean
+
 const clean = () => {
   return del("build");
 };
-
-exports.clean = clean;
 
 // Server
 
@@ -106,43 +103,45 @@ const server = (done) => {
 exports.server = server;
 
 // Reload
+
 const reload = done => {
   sync.reload();
   done();
 }
+
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html", gulp.series(html, sync.reload));
+  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch("source/js/script.js", gulp.series(scripts));
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
-//Build
+// Build
+
 const build = gulp.series(
   clean,
   gulp.parallel(
-  styles,
-  html,
-  scripts,
-  copy,
-  images
-  )
-)
+    styles,
+    html,
+    scripts,
+    copy,
+    images
+  ));
 
 exports.build = build;
 
-const start = gulp.series(
+// Default
+
+exports.default = gulp.series(
   clean,
   gulp.parallel(
-  styles,
-  html,
-  scripts,
-  copy,
+    styles,
+    html,
+    scripts,
+    copy
   ),
   gulp.series(
-  server,
-  watcher
-)
-)
-
-exports.start = start;
+    server,
+    watcher
+  ));
